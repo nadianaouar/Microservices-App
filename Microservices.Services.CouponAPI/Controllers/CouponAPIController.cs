f691e558a7fdcf673ba1c33ff16f10e6b04a96b1
nadia naouar <nadianaouarsfar@gmail.com>
2023-08-14 10:30:15 -0400

    Add CouponAPIController with actions Get() to list coupons and Get(id) to display coupon with specify Id

---
+++ Microservices.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,51 @@
+﻿using Microservices.Services.CouponAPI.Data;
+using Microservices.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        public readonly AppDbContext _db;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db; 
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(c => c.CouponId == id);
+                return coupon;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return null;
+        }
+    }
+}
