110fa9c77e25888704224f05a97d44a770b8c099
nadia naouar <nadianaouarsfar@gmail.com>
2023-08-14 09:31:39 -0400

    Add OnModelCreating() and ApplyMigration()

--- Microservices.Services.CouponAPI/Data/AppDbContext.cs
+++ Microservices.Services.CouponAPI/Data/AppDbContext.cs
@@ -7,9 +7,28 @@ namespace Microservices.Services.CouponAPI.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
+          
         }
         public DbSet<Coupon> Coupons { get; set; }
-    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "100FF",
+                DiscountAmount = 10,
+                MinAmount = 20
+
+            },
+            new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "200FF",
+                DiscountAmount = 20,
+                MinAmount = 40
+
+            });
+        }
     }
 }
--- Microservices.Services.CouponAPI/Program.cs
+++ Microservices.Services.CouponAPI/Program.cs
@@ -1,12 +1,13 @@
 using Microservices.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,5 +28,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigration();
 app.Run();
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+            _db.Database.Migrate();
+    }
+}
